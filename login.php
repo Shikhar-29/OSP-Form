11c8c1873d6389ea7ab4b7fb7fbb116132801492
Shikhar Saxena <shikhar.saxena2015@vit.ac.in>
2017-03-22 22:54:27 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <?php 
-if($_POST[password]=="12345" && ($_POST[regno]=="15BIT0246" || $_POST[regno]=="15bit0246"))
+if($_POST[password]=="12345" && ($_POST[regno]=="15BIT0260" || $_POST[regno]=="15bit0260"))
 {
 	header("location:successful.php");
 	$name=$_POST[name];
